363e815740007c71c53b94fd7adf124658e6d84c
miloudeveen <miloudeveen@gmail.com>
2019-05-22 21:29:05 +0200

    big update
    added images, removed images and made responsive

---
+++ main.js
@@ -0,0 +1,31 @@
+//first try
+function checkKeyPressed(e) {
+    if (e.keyCode == "66") {
+        document.body.style.background = "linear-gradient(magenta, yellow, cyan)";
+        document.body.style.transition = "1.5";
+    }
+}
+
+//second try
+// window.addEventListener("keydown", checkKeyPressed, false);
+
+// function checkKeyPressed(event) {
+//     var element = event.keyCode; 
+//     if (element == "66") {
+//        alert('yo');
+//     }
+// }
+// window.addEventListener('keydown', setClassTimeout('animationTest', 15000, 'animateGradient'), false);
+
+
+
+window.addEventListener('keydown', setClassTimeout, false);
+
+
+function setClassTimeout(e) {
+    let element = document.getElementById('animationTest');
+    if (e.keyCode == "66") {
+        element.classList.add('animateGradient');
+        setTimeout(function(){element.classList.remove('animateGradient')}, 15000);
+    }
+}
